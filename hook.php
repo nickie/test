1b93e2a79fb921ac315fcae034405ad8c13a6822
Nikolaos S. Papaspyrou <nickie@softlab.ntua.gr>
2013-02-15 23:54:57 +0200

    A slightly more clever hook

--- hook.php
+++ hook.php
@@ -1,9 +1,19 @@
 <?php
- 
-// Use in the "Post-Receive URLs" section of your GitHub repo.
- 
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /home/nickie/Projects/release/test && git reset --hard HEAD && git pull && make hook' );
-}
+  GIT_RO="/home/nickie/Projects/release/test";
+  if ($_POST['payload'] && $_POST['magic'] == "42f17bad") {
+    error_reporting(0);
+    try {
+      $payload = json_decode($_POST['payload']);
+    }
+    catch(Exception $e) {
+      exit "Invalid payload!";
+    }
+    if ($payload->ref != 'refs/heads/master') {
+      exit "I only care about the master branch!"; 
+    }
+    shell_exec('cd $GIT_RO && git reset --hard HEAD && git pull && make hook');
+  }
+  else {
+    exit "This should only be used as a hook for github/gitlab."
+  }
 ?>
-git webhook, thanks
